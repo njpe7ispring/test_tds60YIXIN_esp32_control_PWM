e169b159910a781a8dfcfaf7c1d6b60a8ceefa65
njpe7ispring <njpe07.ispring@gmail.com>
2024-11-19 10:00:18 +0800

    增加ignore文件  删减无用注释

--- Hardware/Serial.h
+++ Hardware/Serial.h
@@ -19,11 +19,7 @@ void Serial_Printf(char *format, ...);
 SensorData YIXIN_ParseData(void);
 void YIXIN_SendCommand(void);
 void sendHexData(char *hexString);
-void BA111_Check_callback(void);
-bool BA111_Check_DataReceived(void);
-//void BA111_Getsensordata(void);
-//SensorData BA111_ParseData(void);
-//extern uint8_t Serial_GetDrvFlag(void);
+
 extern uint16_t Serial_TxPacket[8];   //外部使用，用extern，或者用GET函数，指针传递；即可在其他的文件中使用
 extern uint16_t Serial_RxPacket[11];
 uint8_t Serial_GetRxFlag(void);
